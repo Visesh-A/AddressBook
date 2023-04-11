ff8ac890dedaf6730ee7e1b2989cc0bdb33a9582
“Visesh-A” <viseshajl@gmail.com>
2023-04-11 17:49:29 +0530

    [visesh]Add:Contact added

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,28 +1,28 @@
 ﻿using System;
-namespace AddressBook
+namespace AddressBookSystem
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book\r\nProgram in\r\nAddressBookMain class");
-            Contact contact = new Contact();
+            CreateContacts createContacts = new CreateContacts();
             Console.WriteLine("Enter First Name");
-            contact.FirstName = Console.ReadLine();
+            createContacts.FirstName = Console.ReadLine();
             Console.WriteLine("Enetr Last Name");
-            contact.LastName = Console.ReadLine();
+            createContacts.LastName = Console.ReadLine();
             Console.WriteLine("Enter Address");
-            contact.Address = Console.ReadLine();
+            createContacts.Address = Console.ReadLine();
             Console.WriteLine("Enter City");
-            contact.City = Console.ReadLine();
+            createContacts.City = Console.ReadLine();
             Console.WriteLine("Enter State");
-            contact.Satate = Console.ReadLine();
+            createContacts.Satate = Console.ReadLine();
             Console.WriteLine("Enter ZIP");
-            contact.ZIP = Console.ReadLine();
+            createContacts.ZIP = Console.ReadLine();
             Console.WriteLine("Enetr Phone NUmber");
-            contact.PhoneNumber = Console.ReadLine();
+            createContacts.PhoneNumber = Console.ReadLine();
             Console.WriteLine("Enter Email");
-            contact.Email = Console.ReadLine();
+            createContacts.Email = Console.ReadLine();
         }
     }
 }
